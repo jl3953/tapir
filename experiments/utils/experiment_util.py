c3c9ff550b82337d4425aa3a18f9f69885728a7e
jmhelt <jmhelt@users.noreply.github.com>
2021-09-06 15:24:35 -0400

    Fix data agg.

--- experiments/utils/experiment_util.py
+++ experiments/utils/experiment_util.py
@@ -24,15 +24,16 @@ def collect_exp_data(config, remote_exp_directory, local_directory_base, executo
         copy_remote_directory_to_local(os.path.join(
             local_directory_base, 'master'), config['emulab_user'], master_host, remote_directory)
 
-    for shard_idx in range(len(config["shards"])):
-        shard = config["shards"][shard_idx]
-        for replica_idx in range(len(shard)):
-            replica = shard[replica_idx]
-            server_host = get_server_host(config, replica)
-            download_futures.append(executor.submit(copy_remote_directory_to_local, os.path.join(
-                local_directory_base, 'server-%d' % shard_idx), config['emulab_user'], server_host, remote_directory,
-                tar_file="server-{}-{}.tar".format(shard_idx, replica_idx),
-                file_filter="server-{}-{}-*.*".format(shard_idx, replica_idx)))
+    for instance_idx in range(config["num_instances"]):
+        for shard_idx in range(len(config["shards"])):
+            shard = config["shards"][shard_idx]
+            for replica_idx in range(len(shard)):
+                replica = shard[replica_idx]
+                server_host = get_server_host(config, replica)
+                download_futures.append(executor.submit(copy_remote_directory_to_local, os.path.join(
+                    local_directory_base, 'server-%d-%d' % (instance_idx, shard_idx)), config['emulab_user'], server_host, remote_directory,
+                    tar_file="server-{}-{}-{}.tar".format(instance_idx, shard_idx, replica_idx),
+                    file_filter="server-{}-{}-{}-*.*".format(instance_idx, shard_idx, replica_idx)))
 
     for client in config['clients']:
         client_host = get_client_host(config, client)
